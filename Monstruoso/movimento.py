a0b2d405d105e79e0028eab68123a5eb33a8af3a
diogoreisrodrigues <diogo.paulino10@gmail.com>
2022-11-28 16:17:17 +0000

    movimentoTeste

--- Monstruoso/movimento.py
+++ Monstruoso/movimento.py
@@ -10,11 +10,54 @@ ev3 = EV3Brick()
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 
+coord = [1,1]
+dir = "E"
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 
 def movimentoBasico():
     robot.straight(20)
     robot.turn(90)
+
+def move() : 
+    sound = Sound()
+    newCoord = coord
+    if(dir == 'N') :
+        if(coord[1]>1) :
+            robot.turn(180)
+            robot.straight(20)
+            robot.turn(-180)
+            coord[1] = coord[1] -1
+            sound.speak('orait')
+        else:
+            print('fora do tabuleiro')
+    elif(dir == 'S') :
+        if(coord[1] < 6) :
+            robot.straight(20)
+            sound.speak('ok')
+            coord[1] = coord[1] +1
+        else:
+            print('fora do tabuleiro')    
+    elif(dir =='W') :
+        if(coord[0]>1) :
+            robot.turn(-90)
+            robot.straight(20)
+            robot.turn(90)
+            sound.speak('ok')
+            coord[0] = coord[0] -1
+        else:
+            print('fora do tabuleiro')
+    elif(dir == 'E') :
+        if(coord[0]<6) :
+            robot.turn(90)
+            robot.straight(20)
+            robot.turn(-90)
+            sound.speak('orait')
+            coord[0] = coord[0] +1
+        else:
+            print('fora do tabuleiro') 
+    else:
+        sound.speak('Erro')
+    return newCoord
 
 
 while(True):
