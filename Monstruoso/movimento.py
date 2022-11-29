db957143355d3be5d8117358a36beec36deb24ed
diogoreisrodrigues <diogo.paulino10@gmail.com>
2022-11-29 16:52:01 +0000

    diogo commit

--- Monstruoso/movimento.py
+++ Monstruoso/movimento.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env pybricks-micropython
 
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor
+from pybricks.ev3devices import Motor, GyroSensor
 from pybricks.parameters import Port
 from pybricks.robotics import DriveBase
+from pybricks.tools import wait
 from ataques import ataque
 
 # Initialize the EV3 Brick..
@@ -12,18 +13,22 @@ ev3 = EV3Brick()
 # Initialize the motors.
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
+GyroSensor = GyroSensor(Port.S2)
 
 coord = [1,1]
 
-robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+robot = DriveBase(left_motor, right_motor, wheel_diameter=120, axle_track=160)
+robot.settings(400,300,300,300)
 
 def move(dir) : 
     newCoord = coord
     if(dir == 'N') :
         if(coord[1]>1) :
-            robot.turn(370)
+            robot.turn(380)
             robot.straight(420)
-            robot.turn(-370)
+            angulo = GyroSensor.angle() 
+            robot.turn(angulo)
+            GyroSensor.reset_angle(0)
             coord[1] = coord[1] -1
             #sound.speak('orait').
         else:
@@ -39,16 +44,23 @@ def move(dir) :
         if(coord[0]>1) :
             robot.turn(-185)
             robot.straight(420)
-            robot.turn(185)
+            angulo = GyroSensor.angle()
+            robot.turn(angulo)
+            GyroSensor.reset_angle(0)
+
             #sound.speak('ok')
             coord[0] = coord[0] -1
         else:
             print('fora do tabuleiro')
     elif(dir == 'E') :
         if(coord[0]<6) :
-            robot.turn(90)
+            robot.turn(340)
             robot.straight(420)
-            robot.turn(-90)
+            angulo = GyroSensor.angle()  
+            robot.turn(angulo)
+            print(angulo)
+            GyroSensor.reset_angle(0)
+            
             #sound.speak('orait')
             coord[0] = coord[0] +1
         else:
@@ -62,16 +74,22 @@ def move(dir) :
 while(True):
     move("E")
     ataque()
+    wait(100)
     move("S")
     ataque()
+    wait(100)
     move("E")
     ataque()
+    wait(100)
     move("S")
     ataque()
+    wait(100)
     move("W")
     ataque()
+    wait(100)
     move("N")
     ataque()
+    wait(100)
     move("S")
     ataque()
     break
