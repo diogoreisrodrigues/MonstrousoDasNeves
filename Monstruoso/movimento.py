cc331ea43b1e111323a8a61a750a379709c6d4ed
pedroandrade <pedrogsandrade@hotmail.com>
2022-11-29 16:12:31 +0000

    Mudança

--- Monstruoso/movimento.py
+++ Monstruoso/movimento.py
@@ -5,9 +5,10 @@ from pybricks.robotics import DriveBase
 from ataques import ataque
 
 # Initialize the EV3 Brick
+# Initialize the EV3 Brick...
 ev3 = EV3Brick()
 
-# Initialize the motors..
+# Initialize the motors....
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 
@@ -15,37 +16,42 @@ coord = [1,1]
 
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 
-def move(dir) : 
+def movimentoBasico():
+    robot.straight(200)
+    robot.turn(90)
+
+def move() : 
+    sound = Sound()
     newCoord = coord
     if(dir == 'N') :
         if(coord[1]>1) :
-            robot.turn(180)
-            robot.straight(20)
-            robot.turn(-180)
+            robot.turn(370)
+            robot.straight(420)
+            robot.turn(-370)
             coord[1] = coord[1] -1
             #sound.speak('orait').
         else:
             print('fora do tabuleiro')
     elif(dir == 'S') :
         if(coord[1] < 6) :
-            robot.straight(20)
-            sound.speak('ok')
+            robot.straight(420)
+            #sound.speak('ok')
             coord[1] = coord[1] +1
         else:
             print('fora do tabuleiro')    
     elif(dir =='W') :
         if(coord[0]>1) :
-            robot.turn(-90)
-            robot.straight(20)
-            robot.turn(90)
-            sound.speak('ok')
+            robot.turn(-185)
+            robot.straight(420)
+            robot.turn(185)
+            #sound.speak('ok')
             coord[0] = coord[0] -1
         else:
             print('fora do tabuleiro')
     elif(dir == 'E') :
         if(coord[0]<6) :
             robot.turn(90)
-            robot.straight(20)
+            robot.straight(420)
             robot.turn(-90)
             #sound.speak('orait')
             coord[0] = coord[0] +1
@@ -73,3 +79,8 @@ while(True):
     move("S")
     ataque()
     break
+    movimentoBasico()
+
+#TESTE
+
+#aoishdaoihdaoidhaoi
