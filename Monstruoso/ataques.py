090a235b8397afa37e4642c2e881617274e47342
diogoreisrodrigues <diogo.paulino10@gmail.com>
2022-12-16 15:04:32 +0000

    Primeira entrega

--- Monstruoso/ataques.py
+++ Monstruoso/ataques.py
@@ -4,14 +4,12 @@ from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, TouchSensor, ColorSensor
 from pybricks.parameters import Port, Button, Color, Stop
 from pybricks.tools import wait
-from movimento import esperaSensorToque
 
 # The colored objects are either red, green, blue, or yellow.
 POSSIBLE_COLORS = [Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW]
 
 # Initialize the EV3 brick.
 ev3 = EV3Brick()
-
 gripper_motor = Motor(Port.A)
 
 # Initialize the Color Sensor. It is used to detect the color of the objects.
@@ -20,27 +18,8 @@ color_sensor = ColorSensor(Port.S3)
 
 def ataque():
     color = color_sensor.color()
-    if color == Color.GREEN:
-        ev3.speaker.say('take this Zombie')
-        gripper_motor.run_until_stalled(400, then=Stop.HOLD, duty_limit=60)
-        gripper_motor.run_target(200, -50)
-        print("teste1")
+    #if color == Color.GREEN:1
+    gripper_motor.run_until_stalled(400, then=Stop.HOLD, duty_limit=60)
+    gripper_motor.run_target(200, -50)
+    print("teste1")
         
-def pickGear():
-    color = color_sensor.color()
-    if color == Color.BLUE:
-        ev3.speaker.say('I need to run')
-        gripper_motor.run_until_stalled(400, then=Stop.HOLD, duty_limit=60)
-        gripper_motor.run_target(200, -50)
-        print("teste1")
-        while(coord[0] != 6):
-            coord = move(coord, "E")
-            print(coord)
-            ataque()
-            esperaSensorToque()
-        while(coord[1] != 6):
-            coord = move(coord, "S")
-            print(coord)
-            ataque()
-            esperaSensorToque()
-        ev3.speaker.say('Bye Bye motherfuckers')
--- Monstruoso/movimento.py
+++ Monstruoso/movimento.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env pybricks-micropython
 
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor, GyroSensor, UltrasonicSensor, TouchSensor
-from pybricks.parameters import Port
+from pybricks.ev3devices import Motor, GyroSensor, UltrasonicSensor, TouchSensor, ColorSensor
+from pybricks.parameters import Port, Color, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait
-from ataques import ataque, pickGear
+
+from ataques import ataque
+
 
 # Initialize the EV3 Brick..
 ev3 = EV3Brick()
@@ -14,11 +16,18 @@ ev3 = EV3Brick()
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 UltrasonicSensor = UltrasonicSensor(Port.S4)
+gripper_motor = Motor(Port.A)
+
+# Initialize the Color Sensor. It is used to detect the color of the objects.
+color_sensor = ColorSensor(Port.S3)
 
 hasGear = False
+Zombie1 = False
+Zombie2 = False
+NumGear = 0
 
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
-robot.settings(400,300,300,300)
+robot.settings(405,300,300,300)
 
 coord = [1,1]
 
@@ -26,9 +35,9 @@ def move(coord, dir) :
     newCoord = coord
     if(dir == 'N') :
         if(coord[1]>1) :
-            robot.turn(380)
+            robot.turn(405)
             robot.straight(420)
-            robot.turn(-185)
+            robot.turn(-405)
             newCoord[1] = coord[1] -1
             #sound.speak('orait').
         else:
@@ -42,18 +51,18 @@ def move(coord, dir) :
             print('fora do tabuleiro')    
     elif(dir =='W') :
         if(coord[0]>1) :
-            robot.turn(-185)
+            robot.turn(-195)
             robot.straight(420)
-            robot.turn(185)
+            robot.turn(195)
             #sound.speak('ok')
             newCoord[0] = coord[0] -1
         else:
             print('fora do tabuleiro')
     elif(dir == 'E') :
         if(coord[0]<6) :
-            robot.turn(185)
+            robot.turn(195)
             robot.straight(420)
-            robot.turn(-185)
+            robot.turn(-195)
             #sound.speak('orait')
             newCoord[0] = coord[0] +1
         else:
@@ -74,36 +83,211 @@ def esperaSensorToque():
 			else:
 				wait(100)
 
+def pickGear(coord):
+    global NumGear      #Numero de peças que já estão na mota
+    color = color_sensor.color()    
+    if color == Color.BLUE:
+        ev3.speaker.say('I will take this bullet')
+        #gripper_motor.run_until_stalled(405, then=Stop.HOLD, duty_limit=60)
+        #gripper_motor.run_target(200, -50)
+        print("teste1")
+        NumGear = NumGear + 1                   #coloca uma peça na mota
+        print(NumGear)
+        if(NumGear == 2):                       #verifica se já tem as duas peças na mota1
+            while(coord[0] != 6):                   #anda tudo para a esquerda
+                recon(coord)
+                coord = move(coord, "E")
+                print(coord)
+                esperaSensorToque()
+            while(coord[1] != 6):                   #anda tudo para baixo
+                recon(coord)
+                coord = move(coord, "S")
+                print(coord)
+                esperaSensorToque()
+            ev3.speaker.say('Bye Bye motherfuckers')            #se tiver acaba o jogo 
+            print("acabou o jogo")
+            esperaSensorToque()
+        
+
+
+def smell():
+    global Zombie1
+    global Zombie2
+    if(140 <= UltrasonicSensor.distance() <= 405 ):
+        Zombie1 = True
+        Zombie2 = False
+        ev3.speaker.say('I smell you bitch')
+        robot.straight(200)
+        ataque()
+        robot.straight(-200)
+
+    if(450 <= UltrasonicSensor.distance() <= 740 ):
+        Zombie2 = True
+        Zombie1 = False
+        ev3.speaker.say('I see you pendejo')
+    print("Zombie1:"+ str(Zombie1))
+    print("Zombie2:"+ str(Zombie2))
+
+def recon(coord):
+    x = coord[0]
+    y = coord[1]
+
+    if(x == 1 and y == 1 ):
+        smell()  
+        robot.turn(195)
+        smell()
+        robot.turn(-195)
+    elif(x == 1 and 1<y<6):
+        smell()  
+        robot.turn(195)
+        smell()
+        robot.turn(195)
+        smell()
+        robot.turn(-405)
+    elif(x == 1 and y == 6):
+        robot.turn(195)
+        smell()
+        robot.turn(195)
+        smell()
+        robot.turn(-405)
+    elif(1<x<6 and y == 1):
+        smell()  
+        robot.turn(195)
+        smell()
+        robot.turn(405)
+        smell()
+        robot.turn(195)
+    elif(x == 6 and y == 1):
+        smell()
+        robot.turn(-195)
+        smell()
+        robot.turn(195)
+    elif(x == 6 and 1<y<6):
+        smell()
+        robot.turn(-195)
+        smell()
+        robot.turn(-195)  
+        smell()
+        robot.turn(405)
+    elif(x == 6 and y == 6):
+        robot.turn(-195)
+        smell()
+        robot.turn(-195)  
+        smell()
+        robot.turn(405)
+    elif(1<x<6 and y == 6):
+        robot.turn(195)
+        smell()
+        robot.turn(195)  
+        smell()
+        robot.turn(195)
+        smell()
+        robot.turn(195)
+    else:
+        smell()
+        robot.turn(195)
+        smell()
+        robot.turn(195)  
+        smell()
+        robot.turn(195)
+        smell()
+        robot.turn(195)
+
+def pickBullet():
+    color = color_sensor.color()
+    if color == Color.GREEN:
+        ev3.speaker.say('I will take this BULLET')
+        #gripper_motor.run_until_stalled(405, then=Stop.HOLD, duty_limit=60)
+        #gripper_motor.run_target(200, -50)
+
+
 while(True):
     
     while(coord[0] != 6):
-        coord = move(coord, "E")
+        recon(coord)
+        coord = move(coord, "E")                #percorre primeira fila
         print(coord)
-        ataque()
-        pickGear()
+        pickGear(coord)
+        pickBullet()
         esperaSensorToque()
-        
     wait(1000)
-    coord = move(coord, "S")
-    ataque()
-    pickGear()
-    esperaSensorToque()
+    
+    if(coord[1] != 6):
+        recon(coord)
+        coord = move(coord, "S")
+        pickGear(coord)
+        pickBullet()
+        esperaSensorToque()
+    
     while(coord[0] != 1):
-        coord = move(coord, "W")
+        recon(coord)
+        coord = move(coord, "W")                #percorre segunda fila
         print(coord)
-        ataque()
-        pickGear()
+        pickGear(coord)
+        pickBullet()
         esperaSensorToque()
-    wait(1000)
-    coord = move(coord, "S")
-    ataque()
-    pickGear()
-    esperaSensorToque()
+
+    if(coord[1] != 6):
+        recon(coord)
+        coord = move(coord, "S")                #desce para a terceira fila
+        pickGear(coord)
+        pickBullet()
+        esperaSensorToque()
+    
     while(coord[0] != 6):
-        coord = move(coord, "E")
+        recon(coord)
+        coord = move(coord, "E")                #percorre terceira fila
         print(coord)
-        ataque()
-        pickGear()
+        pickGear(coord)
+        pickBullet()
         esperaSensorToque()
     wait(1000)
+    
+    if(coord[1] != 6):
+        recon(coord)
+        coord = move(coord, "S")
+        pickGear(coord)
+        pickBullet()
+        esperaSensorToque()
+    
+    while(coord[0] != 1):
+        recon(coord)
+        coord = move(coord, "W")                #percorre quarta fila
+        print(coord)
+        pickGear(coord)
+        pickBullet()
+        esperaSensorToque()
+
+    if(coord[1] != 6):
+        recon(coord)
+        coord = move(coord, "S")
+        pickGear(coord)
+        pickBullet()
+        esperaSensorToque()
+    
+    while(coord[0] != 6):
+        recon(coord)
+        coord = move(coord, "E")                #percorre quinta fila
+        print(coord)
+        pickGear(coord)
+        pickBullet()
+        esperaSensorToque()
+    wait(1000)
+    
+    if(coord[1] != 6):
+        recon(coord)
+        coord = move(coord, "S")
+        pickGear(coord)
+        pickBullet()
+        esperaSensorToque()
+    
+    while(coord[0] != 1):
+        recon(coord)
+        coord = move(coord, "W")                #percorre sexta fila
+        print(coord)
+        pickGear(coord)
+        pickBullet()
+        esperaSensorToque()
+
     break
+    
